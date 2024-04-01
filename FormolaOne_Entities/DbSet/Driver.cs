e2275904807adcd23a566e295c88aec85977dc3c
Maxruizdz <maxruizdz@hotmail.com>
2024-04-01 11:24:19 -0300

    Agregando entidades

---
+++ FormolaOne_Entities/DbSet/Driver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaOne.Entities.DbSet
+{
+    public class Driver : BaseEntity
+    {
+        public Driver()
+        {
+            achievements = new HashSet<Achievement>();
+        }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public int DriverNumber { get; set; }
+
+
+        public DateTime DateOfBirth { get; set; }
+
+
+        public ICollection<Achievement> achievements { get; set;}
+
+    }
+}
