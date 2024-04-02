b654ee80b5cab677bea085d3e46b74d2f0992a6a
Maxruizdz <maxruizdz@hotmail.com>
2024-04-02 17:04:39 -0300

    Creacion de appContext-primera migracion- Creacion de la base de datos

---
+++ FormulaOne.DataServices/Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using FormulaOne.Entities.DbSet;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaOne.DataServices.Data
+{
+    public  class AppDbContext : DbContext
+    {
+        public virtual DbSet<Driver> Drivers { get; set; }
+
+        public virtual DbSet<Achievement> Achievements { get; set;}
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+            
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Entity<Achievement>(options => { 
+            
+            options.HasOne(p=> p.Driver).WithMany(p=> p.achievements).HasForeignKey(p=>p.DriverId)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("Fk_Achievements_Driver");
+            
+            
+            
+            });
+        }
+    }
+}
--- Into-CQRS-MediatR/Program.cs
+++ Into-CQRS-MediatR/Program.cs
@@ -1,6 +1,21 @@
+using FormulaOne.DataServices.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+{
+    var conexion = builder.Configuration.GetConnectionString("local");
+
+    options.UseSqlServer(conexion);
+
+
+
+});
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
